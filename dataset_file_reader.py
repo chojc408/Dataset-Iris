1a4a14b68a25d84308252953b0215d0682e6da63
Jae Cho <57824135+chojc408@users.noreply.github.com>
2020-08-25 13:59:46 +0900

    Add files via upload

---
+++ dataset_file_reader.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+def csv_to_dataframe(file_name, col_names=None, header=None):
+    # header=0
+    df = pd.read_csv(file_name, names=col_names, header=header)
+    return df
+
+def dataframe_to_array(df):
+    array = pd.DataFrame(df).to_numpy()
+    return array
+
+def array_to_dataframe(array, col_names=None):
+    df = pd.DataFrame(array, columns = col_names)
+    return df
+
+file_name = "iris.data"
+col_names = ["sepal_length", "sepal_width", "petal_length", "petal_width",
+             "scientific_name"]
+
+# === Conversions
+df = csv_to_dataframe(file_name, col_names=col_names, header=None)
+print(df); print()
+print(df.shape); print()
+print(df.head()); print()
+print(list(df.columns)); print()
+
+array = dataframe_to_array(df)
+print(array[:5]); print()
+
+df = array_to_dataframe(array, col_names=col_names)
+print(df); print()
+
+# === Save
+df.to_csv("file_name.csv", index=False, header=True)
+df.to_pickle("file_name.df.pkl")
+np.save("file_name.npy", array)
+
+# === Load
+df = csv_to_dataframe("file_name.csv", header=0) # file_name.csv has a header row.
+print(df.iloc[50:60]); print()
+df = pd.read_pickle("file_name.df.pkl")
+print(df.iloc[50:60]); print()
+array = np.load("file_name.npy", allow_pickle=True)
+print(array[:5])
